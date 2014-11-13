cf8806d3e3ddc0b2eeb80fc33456f59b91e63d57
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-11-13 15:23:25 -0800

    editted commit

---
+++ NYZR/NYZR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NYZR
+//
+//  Created by Ziyuan Liu on 11/13/14.
+//  Copyright (c) 2014 NYZR. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NYZR/NYZR/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NYZR
+//
+//  Created by Ziyuan Liu on 11/13/14.
+//  Copyright (c) 2014 NYZR. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
