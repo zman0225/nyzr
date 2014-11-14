f63238e4734a93195fe40a3ca4e446a15174405f
shellazee <michelleyzheng@gmail.com>
2014-11-14 09:26:00 -0800

    Adds sorting based on 'taxonomy' or category/subject of the .docx

--- alchemySorter.py
+++ alchemySorter.py
@@ -7,19 +7,33 @@ from getText import *
 from alchemyapi import AlchemyAPI
 alchemyapi = AlchemyAPI()
 
-myList = ["Documents/Carmen", "Documents", "Documents/Music Humanities",  "Documents/Econometrics", "Documents/Computer Science Theory"]
+folderList = ["Documents/Carmen", "Documents", "Documents/Music Humanities",  "Documents/Econometrics", "Documents/Computer Science Theory"]
 
-myText2 = get_docx_text("Listening+assignment+4+-+Liszt%2C+Nuages+Gris.docx")
-response = alchemyapi.concepts("text", myText2)
+fileName = "Homework #3 CS Theory.docx"
+myDoc = get_docx_text(fileName)
+response = alchemyapi.concepts("text", myDoc)
+response1 = alchemyapi.taxonomy("text", myDoc)
 
 for concept in response['concepts']:
-       for thing in myList:
-	  thing1 = thing.replace("*/", "")
-          if float(concept['relevance']) > 0.4 and (concept['text'] in thing1
-		or thing1 in concept):
+       for pathName in folderList:
+	  folderName = pathName.replace("*/", "").lower()
+	  conceptL = concept['text'].lower()
+          if float(concept['relevance']) > 0.4 and (conceptL in folderName
+		or folderName in conceptL):
                 print('text: ', concept['text'])
-                print(thing)
+                print("file should be in " + pathName)
                 print('relevance: ', concept['relevance'])
+	  print(conceptL)
 #		shutil.move(os.path.abspath(os.path.join(os.getcwd(), "Listening+assignment+4+-+Liszt%2C+Nuages+Gris.docx"),
 #			os.path.abspath(os.path.join(os.getcwd(), os.path.pardir), os.pardir)))
-print ("Sentiment: " + response['status'])
+
+for taxonomy in response1['taxonomy']:
+      category = taxonomy['label'].replace("*/", "").lower()	
+      category = (category.rsplit('/', 1))[-1].lower()
+#      print(category)
+      for pathName in folderList:
+	   folderName = pathName.replace("*/", "").lower()
+	   if (category in folderName or folderName in category):
+		print("file should be in " + pathName)
+
+#print ("Sentiment: " + response['status'])
