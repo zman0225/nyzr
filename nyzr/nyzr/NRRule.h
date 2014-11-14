22110cf6bfd6b3387753c983f8fa5b2cbe7fc619
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-11-14 07:25:07 -0800

    editted rules

--- nyzr/nyzr/NRRule.h
+++ nyzr/nyzr/NRRule.h
@@ -12,9 +12,9 @@
 @interface NRRule : NSObject <NSCoding, NSCopying>
 @property (nonatomic, strong) NSString *filter;
 @property (nonatomic, strong) NSURL *folderURL;
-@property (nonatomic) NSInteger ruleType;
+@property (nonatomic) NRFilterType ruleType;
 @property (nonatomic) NSUInteger rulePriority;
-@property (nonatomic, strong) NSString *domain;
+
 - (id)initWithFilter:(NSString *)filter folderName:(NSString *)folderName;
 - (BOOL)matchesRule:(NRFile *)file;
 @end
